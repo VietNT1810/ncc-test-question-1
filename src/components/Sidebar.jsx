0ac4474cdf1c113a4b2c1c1990e30ee3b8ccc6df
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-03-07 13:24:05 +0700

    add responsive UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Sidebar.css";
+import useMediaQuery from "../hook/useMediaQuery";
 
 function Sidebar(props) {
+  const matches = useMediaQuery("(max-width: 768px)");
+  const [toggle, setToggle] = useState(false);
+
+  const handleClickToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div className="sidebar">
-      <ul className="navbar">
-        <li className="nav-item">Home</li>
-        <li className="nav-item">Services</li>
-        <li className="nav-item">News</li>
-        <li className="nav-item">Blog</li>
-        <li className="nav-item">Contact</li>
-      </ul>
-    </div>
+    <>
+      {!matches ? (
+        <div className="sidebar">
+          <ul className="navbar">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Services</li>
+            <li className="nav-item">News</li>
+            <li className="nav-item">Blog</li>
+            <li className="nav-item">Contact</li>
+          </ul>
+        </div>
+      ) : (
+        <div className={`mobile-sidebar ${toggle ? "show-nav" : ""}`}>
+          <div className="button-nav">
+            <button className="btn" onClick={handleClickToggle}>
+              <img
+                src={require("../assets/icons/toggle-icon.svg").default}
+                className="button-icon"
+                alt=""
+              />
+            </button>
+          </div>
+          <ul className={`mobile-navbar`}>
+            <li className="mobile-nav-item">Home</li>
+            <li className="mobile-nav-item">Services</li>
+            <li className="mobile-nav-item">News</li>
+            <li className="mobile-nav-item">Blog</li>
+            <li className="mobile-nav-item">Contact</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
